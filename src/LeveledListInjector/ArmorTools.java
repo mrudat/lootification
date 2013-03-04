74785fae5f7d078b8647b036c97a321b431bd073
David <David@PlanEx>
2013-03-04 18:08:26 -0500

    release 0.1.2: Fixed enchanted weap/armo name as subsequence of template name bug.

--- src/LeveledListInjector/ArmorTools.java
+++ src/LeveledListInjector/ArmorTools.java
@@ -267,7 +267,7 @@ public class ArmorTools {
         for (ARMO armor : merger.getArmors()) {
             //SPGlobal.log("armor", armor.getEDID());
             String debugname = armor.getEDID();
-            if(debugname.contains("WV")){
+            if (debugname.contains("WV")) {
                 int qwerty = 1;
             }
             KYWD variantKey = armorHasAnyKeyword(armor, varKeys, merger);
@@ -384,8 +384,13 @@ public class ArmorTools {
             if (baseName.contains(templateName)) {
                 ret = baseName.replace(templateName, name);
             } else {
-                String gcs = longestCommonSubstring(baseName, templateName);
-                ret = baseName.replace(gcs, name);
+                String lcseq = lcs(baseName, templateName);
+                if (baseName.contains(lcseq)) {
+                    ret = baseName.replace(lcseq, name);
+                } else {
+                    String gcs = longestCommonSubstring(baseName, templateName);
+                    ret = baseName.replace(gcs, name);
+                }
             }
         }
 
@@ -421,8 +426,13 @@ public class ArmorTools {
             if (baseName.contains(templateName)) {
                 ret = baseName.replace(templateName, name);
             } else {
-                String gcs = longestCommonSubstring(baseName, templateName);
-                ret = baseName.replace(gcs, name);
+                String lcseq = lcs(baseName, templateName);
+                if (baseName.contains(lcseq)) {
+                    ret = baseName.replace(lcseq, name);
+                } else {
+                    String gcs = longestCommonSubstring(baseName, templateName);
+                    ret = baseName.replace(gcs, name);
+                }
             }
         }
 
@@ -537,7 +547,7 @@ public class ArmorTools {
         for (ArrayList<FormID> vars : armorVariants) {
             //SPGlobal.log("hasVariant", base.getForm() + " " + vars.size());
 //            if (vars.contains(base.getForm()) && (vars.size() > 1)) {
-            if(vars.contains(base.getForm())) {
+            if (vars.contains(base.getForm())) {
                 ret = true;
             }
         }
@@ -579,5 +589,38 @@ public class ArmorTools {
             }
         }
         return S1.substring(Start, (Start + Max));
+    }
+
+    public static String lcs(String a, String b) {
+        int[][] lengths = new int[a.length() + 1][b.length() + 1];
+
+        // row 0 and column 0 are initialized to 0 already
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    lengths[i + 1][j + 1] = lengths[i][j] + 1;
+                } else {
+                    lengths[i + 1][j + 1] = Math.max(lengths[i + 1][j], lengths[i][j + 1]);
+                }
+            }
+        }
+
+        // read the substring out from the matrix
+        StringBuffer sb = new StringBuffer();
+        for (int x = a.length(), y = b.length(); x != 0 && y != 0;) {
+            if (lengths[x][y] == lengths[x - 1][y]) {
+                x--;
+            } else if (lengths[x][y] == lengths[x][y - 1]) {
+                y--;
+            } else {
+                assert a.charAt(x - 1) == b.charAt(y - 1);
+                sb.append(a.charAt(x - 1));
+                x--;
+                y--;
+            }
+        }
+
+        return sb.reverse().toString();
     }
 }
--- src/LeveledListInjector/WeaponTools.java
+++ src/LeveledListInjector/WeaponTools.java
@@ -78,8 +78,13 @@ public class WeaponTools {
             if (baseName.contains(templateName)) {
                 ret = baseName.replace(templateName, name);
             } else {
-                String gcs = longestCommonSubstring(baseName, templateName);
-                ret = baseName.replace(gcs, name);
+                String lcseq = lcs(baseName, templateName);
+                if (baseName.contains(lcseq)) {
+                    ret = baseName.replace(lcseq, name);
+                } else {
+                    String gcs = longestCommonSubstring(baseName, templateName);
+                    ret = baseName.replace(gcs, name);
+                }
             }
         }
 
@@ -241,8 +246,13 @@ public class WeaponTools {
             if (baseName.contains(templateName)) {
                 ret = baseName.replace(templateName, name);
             } else {
-                String gcs = longestCommonSubstring(baseName, templateName);
-                ret = baseName.replace(gcs, name);
+                String lcseq = lcs(baseName, templateName);
+                if (baseName.contains(lcseq)) {
+                    ret = baseName.replace(lcseq, name);
+                } else {
+                    String gcs = longestCommonSubstring(baseName, templateName);
+                    ret = baseName.replace(gcs, name);
+                }
             }
         }
 
@@ -409,5 +419,38 @@ public class WeaponTools {
             }
         }
         return S1.substring(Start, (Start + Max));
+    }
+
+    public static String lcs(String a, String b) {
+        int[][] lengths = new int[a.length() + 1][b.length() + 1];
+
+        // row 0 and column 0 are initialized to 0 already
+
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)) {
+                    lengths[i + 1][j + 1] = lengths[i][j] + 1;
+                } else {
+                    lengths[i + 1][j + 1] = Math.max(lengths[i + 1][j], lengths[i][j + 1]);
+                }
+            }
+        }
+
+        // read the substring out from the matrix
+        StringBuffer sb = new StringBuffer();
+        for (int x = a.length(), y = b.length(); x != 0 && y != 0;) {
+            if (lengths[x][y] == lengths[x - 1][y]) {
+                x--;
+            } else if (lengths[x][y] == lengths[x][y - 1]) {
+                y--;
+            } else {
+                assert a.charAt(x - 1) == b.charAt(y - 1);
+                sb.append(a.charAt(x - 1));
+                x--;
+                y--;
+            }
+        }
+
+        return sb.reverse().toString();
     }
 }
