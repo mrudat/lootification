05384dc9992c67f0f4c230d8f892c588164f5f70
David <dhtynan@gmail.com>
2014-10-06 17:12:20 -0400

    update version

--- src/LeveledListInjector/LeveledListInjector.java
+++ src/LeveledListInjector/LeveledListInjector.java
@@ -75,7 +75,7 @@ public class LeveledListInjector implements SUM {
     };
     public static String myPatchName = "LLI";
     public static String authorName = "Dienes";
-    public static String version = "0.6.4";
+    public static String version = "0.6.5";
     public static String welcomeText = "Lootifies weapons and armors";
     public static String descriptionToShowInSUM = "Lootify weapons and armor.";
     public static Color headerColor = new Color(66, 181, 184);  // Teal
